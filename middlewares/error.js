bc845dddb26d3d57918db7db6a9089c2cb82e28f
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-29 09:25:15 +0530

    some issue

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,5 @@
 export const errorMiddleware = (err, req, res, next) => {
-  err.message = err.message || "Internal Server Error";
+  err.message = err
   err.statusCode = err.statusCode || 500;
 
   if (err.code === 11000) {
